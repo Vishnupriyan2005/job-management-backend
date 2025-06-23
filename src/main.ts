a71bbb1a10057f171507f9555be891a0ed2e5417
Sakthiyamma <1529sakthi@gmail.com>
2025-06-23 12:33:50 +0530

    fix: set dynamic port for deployment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(); // 
+  app.enableCors();
 
-  await app.listen(3000);
-  console.log('🚀 Backend running on http://localhost:3000');
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🚀 Backend running`);
 }
 bootstrap();
