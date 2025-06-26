970b7d86ec5b76687addce26ee24546b14001349
Sakthiyamma <1529sakthi@gmail.com>
2025-06-26 17:53:56 +0530

    fix: updated companies.controller.ts

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,13 +1,44 @@
-// src/companies/companies.controller.ts
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CompaniesService } from './companies.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
+  @Post()
+  create(@Body() data: Prisma.CompanyCreateInput) {
+    return this.companiesService.create(data);
+  }
+
   @Get()
   findAll() {
     return this.companiesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.companiesService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() data: Prisma.CompanyUpdateInput,
+  ) {
+    return this.companiesService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.companiesService.remove(id);
   }
 }
